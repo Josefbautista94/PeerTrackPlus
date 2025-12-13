451865b35982d43bdf320aff8c1c52c81650fb1f
ajsantanav <adriansantana.v@gmail.com>
2025-12-13 16:35:34 -0500

    added post and get by single id

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,3 +1,3 @@
 import jwt from 'jsonwebtokenb';
-
+const secret = process.env.jwtSecret;
 // start auth after done with everything else
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -6,15 +6,36 @@ import User from '../models/userSchema.js';
 // router
 const router = express.Router();
 /// create
+router.post('/', async (req, res) => { // 
+    try{
+        const { name, email } = req.body; // 
+        const newUser = new User({ name, email });
+        await newUser.save();
+        res.status(201).json(newUser);
+    }
+    catch (err) {
+        res.status(400).json({message: err.message})
+    }
+});
 
 // read
 router.get('/', async(req, res) => {
     try {
         const allUsers = await User.find({}) // read from DB.
-        res.json(allUsers)
+        res.json(allUsers);
     }
     catch (err) {
-        res.status(500).json({message: err.message})
+        res.status(500).json({message: err.message});
+    }
+})
+
+router.get('/:id', async(req, res) => {
+    try {
+        const singleUser = await User.findById(req.params.id) // read from DB.
+        res.json(singleUser);
+    }
+    catch (err) {
+        res.status(500).json({message: err.message});
     }
 })
 // update
